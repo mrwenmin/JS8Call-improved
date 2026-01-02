e56ab79f6a610632714d0bed8b6b6667302ee1ce
Chris-AC9KH <chriskristin.olson@icloud.com>
2026-01-02 17:39:39 -0600

    Branch code to release/2.5.0

--- Versions.cmake
+++ Versions.cmake
@@ -2,4 +2,4 @@
 set (WSJTX_VERSION_MAJOR 2)
 set (WSJTX_VERSION_MINOR 5)
 set (WSJTX_VERSION_PATCH 0)
-set (WSJTX_VERSION_IS_RELEASE 0) # set to 1 for final release build
+set (WSJTX_VERSION_IS_RELEASE 1) # set to 1 for final release build
