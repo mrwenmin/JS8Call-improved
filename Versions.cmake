f0f0d01b357c8eb8786aee687e8b5c787c787159
Chris-AC9KH <chriskristin.olson@icloud.com>
2026-01-25 12:52:10 -0600

    bump to 2.5.2 patch release

--- Versions.cmake
+++ Versions.cmake
@@ -1,5 +1,5 @@
 # Version number components
 set (WSJTX_VERSION_MAJOR 2)
 set (WSJTX_VERSION_MINOR 5)
-set (WSJTX_VERSION_PATCH 1)
+set (WSJTX_VERSION_PATCH 2)
 set (WSJTX_VERSION_IS_RELEASE 1) # set to 1 for final release build
